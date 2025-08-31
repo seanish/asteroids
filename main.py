02c57edfd7a00a03664c99e02ea516e470a6ee2a
seanish <sean.mallia@icloud.com>
2025-08-31 10:18:52 -0400

    Setting FPS to 60

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ def main():
     pygame.init()
     print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60)
 
 
 if __name__ == "__main__":
