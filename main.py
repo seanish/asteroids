442acdabd7739516dd77aa6cbc4f90da0706271a
seanish <sean.mallia@icloud.com>
2025-08-31 10:53:50 -0400

    Fix

--- main.py
+++ main.py
@@ -19,8 +19,7 @@ def main():
         player.draw(screen)
         player.update(dt)
         pygame.display.flip()
-        clock.tick(60)
-        dt = clock.tick() / 1000
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
